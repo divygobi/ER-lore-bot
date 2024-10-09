e860b2e73093037437fc618e81118d3b2839ae6b
Divy Gobiraj <divygobi@gmail.com>
2024-10-08 20:57:51 -0700

    test

--- client/erlorebot/src/app/page.tsx
+++ client/erlorebot/src/app/page.tsx
@@ -1,10 +1,11 @@
 import { ChatInput } from "@/components/ui/chat/chat-input"
 
+//test
 export default function Home() {
   return (
    <>
    
-
+    
 
 <ChatInput
   placeholder="Type your message here..."
