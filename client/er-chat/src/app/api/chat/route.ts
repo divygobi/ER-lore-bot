83c41e0229e50f89201ebda777bf42f2009e652e
Divy Gobiraj <divygobi@gmail.com>
2024-10-23 21:32:17 -0700

    cleaned up chat component

--- client/er-chat/src/app/api/chat/route.ts
+++ client/er-chat/src/app/api/chat/route.ts
@@ -1,14 +1,15 @@
-import { openai } from "@ai-sdk/openai";
 import { streamText, convertToCoreMessages } from "ai";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const result = await streamText({
-    model: openai("gpt-4-turbo"),
+    model: genAI.getGenerativeModel({ model: "gemini-1.5-flash" }),
     messages: convertToCoreMessages(messages),
   });
 
--- client/er-chat/src/app/page.tsx
+++ client/er-chat/src/app/page.tsx
@@ -20,7 +20,6 @@ import {
 } from "lucide-react";
 import { useChat } from "ai/react";
 import { useEffect, useRef, useState } from "react";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import CodeDisplayBlock from "@/components/code-display-block";
@@ -115,52 +114,9 @@ export default function Home() {
         {/* Initial Message */}
         {messages.length === 0 && (
           <div className="w-full bg-background shadow-sm border rounded-lg p-8 flex flex-col gap-2">
-            <h1 className="font-bold">Welcome to this example app.</h1>
-            <p className="text-muted-foreground text-sm">
-              This is a simple Next.JS example application created using{" "}
-              <a
-                href="https://github.com/jakobhoeg/shadcn-chat"
-                className="font-bold inline-flex flex-1 justify-center gap-1 leading-4 hover:underline"
-              >
-                shadcn-chat
-                <svg
-                  aria-hidden="true"
-                  height="7"
-                  viewBox="0 0 6 6"
-                  width="7"
-                  className="opacity-70"
-                >
-                  <path
-                    d="M1.25215 5.54731L0.622742 4.9179L3.78169 1.75597H1.3834L1.38936 0.890915H5.27615V4.78069H4.40513L4.41109 2.38538L1.25215 5.54731Z"
-                    fill="currentColor"
-                  ></path>
-                </svg>
-              </a>{" "}
-              components. It uses{" "}
-              <a
-                href="https://sdk.vercel.ai/"
-                className="font-bold inline-flex flex-1 justify-center gap-1 leading-4 hover:underline"
-              >
-                Vercel AI SDK
-                <svg
-                  aria-hidden="true"
-                  height="7"
-                  viewBox="0 0 6 6"
-                  width="7"
-                  className="opacity-70"
-                >
-                  <path
-                    d="M1.25215 5.54731L0.622742 4.9179L3.78169 1.75597H1.3834L1.38936 0.890915H5.27615V4.78069H4.40513L4.41109 2.38538L1.25215 5.54731Z"
-                    fill="currentColor"
-                  ></path>
-                </svg>
-              </a>{" "}
-              for the AI integration. Build chat interfaces like this at
-              lightspeed with shadcn-chat.
-            </p>
-            <p className="text-muted-foreground text-sm">
-              Make sure to also checkout the shadcn-chat support component at
-              the bottom right corner.
+            <h1 className="font-bold">Welcome to this lore bot</h1>
+            <p>
+              Ask any question about Elden ring and I will try to answer it, only using canon sources.
             </p>
           </div>
         )}
@@ -267,29 +223,7 @@ export default function Home() {
           </div>
         </form>
       </div>
-      <div className="pt-4 flex gap-2 items-center">
-        <GitHubLogoIcon className="size-4" />
-        <p className="text-xs">
-          <a
-            href="https://github.com/jakobhoeg/shadcn-chat"
-            className="font-bold inline-flex flex-1 justify-center gap-1 leading-4 hover:underline"
-          >
-            shadcn-chat
-            <svg
-              aria-hidden="true"
-              height="7"
-              viewBox="0 0 6 6"
-              width="7"
-              className="opacity-70"
-            >
-              <path
-                d="M1.25215 5.54731L0.622742 4.9179L3.78169 1.75597H1.3834L1.38936 0.890915H5.27615V4.78069H4.40513L4.41109 2.38538L1.25215 5.54731Z"
-                fill="currentColor"
-              ></path>
-            </svg>
-          </a>
-        </p>
-      </div>
+     
     </main>
   );
 }
