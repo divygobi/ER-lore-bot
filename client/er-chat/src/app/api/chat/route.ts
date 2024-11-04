f694674ca972028b235e712a4f429787ba82c7eb
Divy Gobiraj <divygobi@gmail.com>
2024-11-04 10:08:36 -0800

    Working prototype

--- client/er-chat/src/app/api/chat/route.ts
+++ client/er-chat/src/app/api/chat/route.ts
@@ -10,6 +10,7 @@ const dotenv = require('dotenv');
 dotenv.config({ path: '/Users/divygobiraj/Desktop/projects/ER_Lore_Bot/ER-lore-bot/.env' });
 
 
+
 const google = createGoogleGenerativeAI(
   {apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY,
   headers: {}}
@@ -38,12 +39,40 @@ export async function POST(req: Request) {
   
   console.log(documents);
 
+  messages[messages.length - 1].content = contextualize(mostRecentMessage, documents);
+
   const result = await streamText({
     model: google('gemini-1.5-flash'),
     messages: convertToCoreMessages(messages),
   });
 
   return result.toDataStreamResponse();
+}
+
+function contextualize(message: String, documents: any) {
+
+  var instructions: string = "INSTRUCTIONS: \n";
+  instructions = instructions + "1. Read the documents below. \n";
+  instructions = instructions + "2. They are in game text of Elden Ring lore. These are canon sources, do your best to really cite them.\n";
+  instructions = instructions + "3. Use the information in the documents to answer the question. \n";
+
+  var documentsString: string = "DOCUMENTS: \n";
+
+  for (var i = 0; i < documents.length; i++) {
+    documentsString = documentsString + JSON.stringify(documents[i]);
+    documentsString += "\n";
+  }
+  
+  var messageString: string = "QUESTION: \n";
+  messageString = messageString + message;
+
+  console.log(instructions + documentsString + messageString);
+  
+
+  return instructions + documentsString + messageString;
+
+
+
 }
 
 
